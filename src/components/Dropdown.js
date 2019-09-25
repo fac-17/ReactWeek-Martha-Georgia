a26fa33fb92ed969cb3c57455f3d0549fe699788
georgia.shaw <georgia@cranbrookw4.com>
2019-09-25 15:40:07 +0100

    start dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dropdown = () => {
+  const [category, setCategory] = React.useState(null);
+  React.useEffect(() => {});
+  return (
+    <div>
+      <select>
+        <option value="9" />
+      </select>
+    </div>
+  );
+};
